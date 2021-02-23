e1c91d80ca8e75ff3e36faad25a72b317cede82e
Junaire Edris B. Buico <juned.buico@gmail.com>
2021-02-23 13:15:03 +0800

    edited

--- src/portfolio.js
+++ src/portfolio.js
@@ -146,7 +146,7 @@ const techStack = {
 // Work experience section
 
 const workExperiences = {
-	display: true, //Set it to true to show workExperiences Section
+	display: false, //Set it to true to show workExperiences Section
 	experience: [
 		{
 			role: 'Software Engineer',
